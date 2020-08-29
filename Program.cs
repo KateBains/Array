eacc805491a506dfe236343fece578912cb931c5
KateBains <katebains1@gmail.com>
2020-08-29 13:57:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿ using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+           
+            string[] friends = new string[2];
+            friends[0] = "rosa";
+            friends[1] = "mawiya";
+            
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(friends[1]);
+            Console.ReadLine();
+        }
+    }
+}
